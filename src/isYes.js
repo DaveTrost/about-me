c3b7c78c598b614da80ba061792398e02cf045f4
Dave Trost <davetrost@gmail.com>
2019-07-16 09:49:48 -0700

    isYes function built and passing all tests

---
+++ src/isYes.js
@@ -0,0 +1,4 @@
+function isYes(userInput) {
+    return ((userInput.trim()).toLowerCase()[0] === 'y');
+}
+export default isYes;
--- test/tests.js
+++ test/tests.js
@@ -1,12 +1,59 @@
+import isYes from '../src/isYes.js';
 const test = QUnit.test;
 
-test('time to test a function', function(assert) {
+test('Test mixed-case \'Yes\'', function(assert) {
     //Arrange
     // Set up your parameters and expectations
+    const text = 'Yes';
+    const expected = true;
 
     //Act 
     // Call the function you're testing and set the result to a const
+    const result = isYes(text);
 
     //Assert
-    assert.equal(true, false);
+    assert.equal(result, expected);
 });
+
+test('Test upper-case \'YES\'', function(assert) {
+    //Arrange
+    // Set up your parameters and expectations
+    const text = 'YES';
+    const expected = true;
+
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const result = isYes(text);
+
+    //Assert
+    assert.equal(result, expected);
+});
+
+test('Test lowercase \'yes\'', function(assert) {
+    //Arrange
+    // Set up your parameters and expectations
+    const text = 'yes';
+    const expected = true;
+
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const result = isYes(text);
+
+    //Assert
+    assert.equal(result, expected);
+});
+
+test('Test a negative response \'no\'', function(assert) {
+    //Arrange
+    // Set up your parameters and expectations
+    const text = 'no';
+    const expected = false;
+
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const result = isYes(text);
+
+    //Assert
+    assert.equal(result, expected);
+});
+
