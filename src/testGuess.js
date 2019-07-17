ad26c00a10fb7eb9bc71476107de59b95ebdf135
Dave Trost <davetrost@gmail.com>
2019-07-17 13:33:00 -0700

    stretch goal: throw errors for incorrect user inputs, add bad input cases to unit tests

--- src/testGuess.js
+++ src/testGuess.js
@@ -1,15 +1,10 @@
 function testGuess(userInput, goal) {
-    try {
-        if(userInput === '') throw 'empty';
-        if(isNaN(userInput)) throw 'NaN';
+    if(userInput === '') throw 'error: userInput is empty';
+    if(isNaN(userInput)) throw 'error: userInput is NaN';
 
-        const userInputAsNumber = Number(userInput);
-        if(userInputAsNumber === goal) return 0;
-        else if(userInputAsNumber < goal) return -1;
-        else return 1;
-    }
-    catch(err) {
-        MessageChannel.innerHTML = 'Error, user input is ' + err;
-    }
+    const userInputAsNumber = Number(userInput);
+    if(userInputAsNumber === goal) return 0;
+    else if(userInputAsNumber < goal) return -1;
+    else return 1;
 }
 export default testGuess;
